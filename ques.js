dc5ef9f0a3cffaf26ba191212f3f40ab6c0c0a83
Ajay Pandey <57106804+panditajay73@users.noreply.github.com>
2022-05-24 22:16:45 +0530

    Add files via upload

---
+++ ques.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+     {
+     numb: 6,
+     question: "The correct sequence of HTML tags for starting a webpage is",
+     answer: "HTML, Head, Title, Body",
+     options: [
+       "Head, Title, HTML, body",
+       "HTML, Body, Title, Head",
+       "HTML, Title, Head, Body",
+       "HTML, Head, Title, Body"
+     ]
+   },
+   {
+   numb: 7,
+   question: "Which of the following element is responsible for making the text bold in HTML?",
+   answer: "b",
+   options: [
+     "pre",
+     "a",
+     "b",
+     "br"
+   ]
+ },
+ {
+ numb: 8,
+ question: " Which of the following tag is used to define options in a drop-down selection list?",
+ answer: "option",
+ options: [
+   "select",
+   "list",
+   "dropdown",
+   "option"
+ ]
+},
+{
+numb: 9,
+question: " What are the types of unordered or bulleted list in HTML?",
+answer: "disc, circle, square",
+options: [
+  "disc, square, triangle",
+  "polygon, triangle, circle",
+  "disc, circle, square",
+  "All of the above"
+]
+},
+{
+numb: 10,
+question: " input is ",
+answer: "an empty tag",
+options: [
+  "a format tag",
+  "an empty tag",
+  "All of the above",
+  "None of the above"
+]
+},
+];
